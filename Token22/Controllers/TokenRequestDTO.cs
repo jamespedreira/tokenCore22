365d62c3fd0015c41b16dd9bd56a7c072de85bb6
jamespedreira <49241005+jamespedreira@users.noreply.github.com>
2019-04-09 16:20:57 -0300

    Add files via upload

---
+++ Token22/Controllers/TokenRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Geriatria.Api.DTOs
+{
+    public class TokenRequestDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Grant_Type { get; set; }
+    }
+}
